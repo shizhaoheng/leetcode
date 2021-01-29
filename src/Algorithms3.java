1911f6561bb624858757645dd349f0d9c2e2a6e9
biubiubiu <2870400383@qq.com>
2021-01-29 20:52:26 +0800

    修改

--- src/Algorithms3.java
+++ src/Algorithms3.java
@@ -1,49 +1,69 @@
-import java.util.Date;
-
 /**
  * @Author : shi zhao heng
- * @Date : 2020/11/4 15:22
- * @Description :
+ * @Date : 2020/10/29 14:09
+ * @Description :无重复字符的最长子串
  */
 
+import java.util.HashSet;
+import java.util.Set;
 
-/*
-4. 寻找两个正序数组的中位数   难度-困难
-给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
-进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+ * 示例 1:
 
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
 
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
 
-示例 1：
-
-输入：nums1 = [1,3], nums2 = [2]
-输出：2.00000
-解释：合并数组 = [1,2,3] ，中位数 2
-示例 2：
-
-输入：nums1 = [1,2], nums2 = [3,4]
-输出：2.50000
-解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
-示例 3：
-
-输入：nums1 = [0,0], nums2 = [0,0]
-输出：0.00000
-示例 4：
-
-输入：nums1 = [], nums2 = [1]
-输出：1.00000
-示例 5：
-
-输入：nums1 = [2], nums2 = []
-输出：2.00000
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  */
-    /*
-
-     */
-
 public class Algorithms3 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        String s = "abcabcbb";
+        int result = lengthOfLongestSubstring(s);
+        System.out.println(result);
+        long end = System.currentTimeMillis();
+        System.out.println("程序耗时：" + (end - start) + "毫秒" );
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {//
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+
+        }
+        return ans;
+
 
     }
+
+
 }
